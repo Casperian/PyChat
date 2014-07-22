76dbdd15f829722bc9d4545f27ae08639bb4baf5
Casperian <Casperian.Anderson@gmail.com>
2014-07-22 14:07:17 +1200

    Dedicated Version 0.9.18.428
    New Version with updated code. Previous commit was a older version that
    I uploaded as the current version.

--- DedicatedServer.py
+++ DedicatedServer.py
@@ -1,25 +1,124 @@
 # Echo server program
 import socket,select
+from datetime import datetime
 from tkinter import *
-print("Server IP")
-print(socket.gethostbyname(socket.gethostname()))
+
 HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
+PORT = 20202            # Arbitrary non-privileged port
+userlist = []
+
+try:
+    with open("MOTD.txt", 'rb') as fo:
+        motd = fo.read()
+except:
+    print("Error with MOTD.txt - Does it exist?")
+    motd = "Hello User"
+    motd = motd.encode('utf-8')
+    
+hlplst = []
+hlplst.append("Commands are proceded by a /")
+hlplst.append("Currently the working commands are;")
+hlplst.append("/ip")
+hlplst.append("/about")
+
+
+print("Server IP")
+print(socket.gethostbyname(socket.gethostname())+ ":" + str(PORT))
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
+s.listen(10)
+userlist.append(s)
+
+#Function to broadcast chat messages to all connected clients
+def broadcast (read, message):
+    #Do not send the message to master socket and the client who has send us the message
+    for socket in userlist:
+        if socket != s: # and socket != read :
+            try :
+                socket.send(message)
+            except :
+                # broken socket connection may be, chat client pressed ctrl+c for example
+                socket.close()
+                userlist.remove(socket)
+
+def wlcmusr (read, message):
+    #Do not send the message to master socket and the client who has send us the message
+    for socket in userlist:
+        if socket != s and socket != read :
+            try :
+                socket.send(message)
+            except :
+                # broken socket connection may be, chat client pressed ctrl+c for example
+                socket.close()
+                userlist.remove(socket)                
+try:
+    while True:
+        read_sockets,write_sockets,error_sockets = select.select(userlist,[],[])
+        for read in read_sockets:
+            if read == s:
+                conn, addr = s.accept()
+                print("New User: " + str(addr))
+                userlist.append(conn)
+
+            else:
+                try:
+                    data = read.recv(8192)
+                    if data:
+                        dedata = data.decode('utf-8')
+                        if ">" in dedata:
+                            param, value = dedata.split(">",1)
+                            if value == "/ip":
+                                ip = "IP: " + socket.gethostbyname(socket.gethostname())
+                                ip = ip.encode('utf-8')
+                                read.send(ip)
+
+                            elif value == "/help":
+                                for i in range(0,4):
+                                    hlpmssg = hlplst[i]
+                                    hlpmssg = hlpmssg.encode('utf-8')
+                                    read.send(hlpmssg)
+                                    time.sleep(0.05)
+
+                            elif value == "/about":
+                                about = "PyChat Server Version 0.9.18.434" 
+                                about = about.encode('utf-8')
+                                read.send(about)
 
 
-while True:
-    ready = select.select([conn], [], [], 0)
-    if ready[0]:
-        data = conn.recv(8192)
-        if data:
-            conn.sendall(data)
 
-conn.close()
+
+
+                                
+                            else:
+                                ctime = str(datetime.now().time())
+                                ctime = "[" + ctime[:5] + "] "
+                                ctime = ctime.encode('utf-8')
+                                transdata = ctime + data
+                                with open("Log.txt", 'wb') as fo:
+                                    fo.write(transdata)
+                                broadcast(read,transdata)
+
+                        else:
+                            ctime = str(datetime.now().time())
+                            ctime = "[" + ctime[:5] + "] "
+                            ctime = ctime.encode('utf-8')
+                            transdata = ctime + data                            
+                            read.send(motd)
+                            wlcmusr(read,transdata)
+
+                except:
+                    print("Connection Error from client", read)
+                    read.close()
+                    userlist.remove(read)
+                    continue
+except(KeyboardInterrupt):
+    print("Shutting down at user command")
+    s.shutdown(Socket.SHUT_WR)
+    time.sleep(1)
+    s.close()
 
 
 
--- Paired Client.py
+++ Paired Client.py
@@ -1,12 +1,38 @@
 # Echo server program
+from win32api import GetSystemMetrics
+import socket, select, random
 import socket, select
 from tkinter import *
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 loop = 0
+gotuser = 0
+def getuser():
+    global gotuser
+    global username
+    usernamesuffix = str(random.randint(1000,9999))
+    tempuser = userBox.get()
+    if tempuser == "":
+       Lb1.insert(0, "Please Select a Username")
+    else:
+        gotuser = 1
+        username = tempuser + "." + usernamesuffix
+
 def recieveData():
     global loop
     if loop > 10:
         loop = 0
+    try:    
+        ready = select.select([s], [], [], 0)
+        if ready[0]:
+            data = s.recv(4096)
+            data2 = data.decode('utf-8')
+            if data:
+                    Lb1.insert(loop, data2)
+                    loop = loop + 1
+        app.after(1, recieveData)
+    except:
+        print("Socket Down")
     ready = select.select([s], [], [], 0)
     if ready[0]:
         data = s.recv(4096)
@@ -17,22 +43,44 @@ def recieveData():
     app.after(1, recieveData)
 
 def closeapp():
+    try:
+        s.shutdown(socket.SHUT_WR)
+        exit()
+    except:        
+        exit()
     s.close()
 
 def sendData():
     global text
     text = str(entryBox.get())
+    text2 = "<" + username + ">" + text
+    text3 = text2.encode('utf-8')
+    s.sendall(text3)
     text2 = text.encode('utf-8')
     s.sendall(text2)
 
 def connserver():
+    if gotuser == 0:
+        getuser()
+    connection = connectBox.get()
+    if ":" in connection:
+        param, value = connection.split(":",1)
+        PORT = value
+        HOST = param
+    else:
+        HOST = connection
+        PORT = 20202             # The same port as used by the server
     HOST = connectBox.get()    # The remote host
     PORT = 50007              # The same port as used by the server
     try:
         s.connect((HOST, PORT))
+        welcome = username + " joined the chat"
+        welcome = welcome.encode('utf-8')
+        s.sendall(welcome)
 
     except:
         Lb1.insert(0, "Connection Failed")
+        raise
     confirm = str("Connected")
     confirmen = confirm.encode('utf-8')
     s.sendall(confirmen)
@@ -41,27 +89,47 @@ def connserver():
 
 app = Tk()
 app.title("")
+app.resizable(0,0)
+width = (GetSystemMetrics(0))//2
+height = (GetSystemMetrics(1))//2
+res = str(width) + 'x' + str(height)
+app.geometry(res)
 app.geometry("500x500")
 
 connectBox = Entry(app)
+connectBox.place(x=60,y=25)
 connectBox.pack()
 connectBox.focus_set()
 
 connButton = Button(app, text = "Connect", command = connserver)
+connButton.place(x=0,y=50)
 connButton.pack()
 
+entryBox = Entry(app,width=145)
+entryBox.place(x=10,y=(height-80))
 entryBox = Entry(app)
 entryBox.pack()
 
 sendButton = Button(app, text = "Send", command = sendData)
+sendButton.place(x=(width-60),y=(height-85))
 sendButton.pack()
 
+Lb1 = Listbox(app, height=20, width=150)
+Lb1.place(x=20,y=100)
 Lb1 = Listbox(app)
 Lb1.pack()
 
 recieveData()
+userBox = Entry(app)
+userBox.place(x=60,y=0)
+userBox.focus_set()
+
+userlabel = Label(app, text="Username").place(x=0,y=0)
+
+serverlabel = Label(app, text="Server").place(x=0,y=25)
 
 closeButton = Button(app, text = "Release", command = closeapp)
+closeButton.place(x=(width-60),y=(height-40))
 closeButton.pack()
 
 app.mainloop()
