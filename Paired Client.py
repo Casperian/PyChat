82e662783a409a4d305a3d09183735a3266f101d
Casperian <Casperian.Anderson@gmail.com>
2014-07-22 13:49:40 +1200

    Dedicated Version 0.9.18.428
    New Versions Uploaded. Changed in Readme.

--- Paired Client.py
+++ Paired Client.py
@@ -1,68 +1,107 @@
 # Echo server program
-import socket, select
+from win32api import GetSystemMetrics
+import socket, select, random
 from tkinter import *
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 loop = 0
+gotuser = 0
+def getuser():
+    global gotuser
+    global username
+    usernamesuffix = str(random.randint(1000,9999))
+    tempuser = userBox.get()
+    if tempuser == "":
+       Lb1.insert(0, "Please Select a Username")
+    else:
+        gotuser = 1
+        username = tempuser + "." + usernamesuffix
+
 def recieveData():
     global loop
     if loop > 10:
         loop = 0
-    ready = select.select([s], [], [], 0)
-    if ready[0]:
-        data = s.recv(4096)
-        data2 = data.decode('utf-8')
-        if data:
-                Lb1.insert(loop, data2)
-                loop = loop + 1
-    app.after(1, recieveData)
-
+    try:    
+        ready = select.select([s], [], [], 0)
+        if ready[0]:
+            data = s.recv(4096)
+            data2 = data.decode('utf-8')
+            if data:
+                    Lb1.insert(loop, data2)
+                    loop = loop + 1
+        app.after(1, recieveData)
+    except:
+        print("Socket Down")
 def closeapp():
-    s.close()
+    try:
+        s.shutdown(socket.SHUT_WR)
+        exit()
+    except:        
+        exit()
 
 def sendData():
-    global text
     text = str(entryBox.get())
-    text2 = text.encode('utf-8')
-    s.sendall(text2)
+    text2 = "<" + username + ">" + text
+    text3 = text2.encode('utf-8')
+    s.sendall(text3)
 
 def connserver():
-    HOST = connectBox.get()    # The remote host
-    PORT = 50007              # The same port as used by the server
+    if gotuser == 0:
+        getuser()
+    connection = connectBox.get()
+    if ":" in connection:
+        param, value = connection.split(":",1)
+        PORT = value
+        HOST = param
+    else:
+        HOST = connection
+        PORT = 20202             # The same port as used by the server
     try:
         s.connect((HOST, PORT))
+        welcome = username + " joined the chat"
+        welcome = welcome.encode('utf-8')
+        s.sendall(welcome)
 
     except:
         Lb1.insert(0, "Connection Failed")
-    confirm = str("Connected")
-    confirmen = confirm.encode('utf-8')
-    s.sendall(confirmen)
+        raise
 
 
 
 app = Tk()
 app.title("")
-app.geometry("500x500")
+app.resizable(0,0)
+width = (GetSystemMetrics(0))//2
+height = (GetSystemMetrics(1))//2
+res = str(width) + 'x' + str(height)
+app.geometry(res)
 
 connectBox = Entry(app)
-connectBox.pack()
-connectBox.focus_set()
+connectBox.place(x=60,y=25)
 
 connButton = Button(app, text = "Connect", command = connserver)
-connButton.pack()
+connButton.place(x=0,y=50)
 
-entryBox = Entry(app)
-entryBox.pack()
+entryBox = Entry(app,width=145)
+entryBox.place(x=10,y=(height-80))
 
 sendButton = Button(app, text = "Send", command = sendData)
-sendButton.pack()
+sendButton.place(x=(width-60),y=(height-85))
 
-Lb1 = Listbox(app)
-Lb1.pack()
+Lb1 = Listbox(app, height=20, width=150)
+Lb1.place(x=20,y=100)
 
 recieveData()
+userBox = Entry(app)
+userBox.place(x=60,y=0)
+userBox.focus_set()
+
+userlabel = Label(app, text="Username").place(x=0,y=0)
+
+serverlabel = Label(app, text="Server").place(x=0,y=25)
 
 closeButton = Button(app, text = "Release", command = closeapp)
-closeButton.pack()
+closeButton.place(x=(width-60),y=(height-40))
 
 app.mainloop()
 
